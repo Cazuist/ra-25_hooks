39ed83226c851f7e7764de2a8b9dbcf2fe0c4b5c
Cazuist <sovrus@mail.ru>
2020-12-28 12:37:49 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import UsersWidget from './components/list/UsersWidget';
+import CustomHookWidget from './components/custom/CustomHookWidget';
 
 function App() {
+  const url = process.env.REACT_APP_URL;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>      
+      <div className="task-title">useEffect</div>
+      <UsersWidget url={url}/>
+
+      <div className="task-title">useJsonFetch</div>
+      <CustomHookWidget />
+    </>    
   );
 }
 
